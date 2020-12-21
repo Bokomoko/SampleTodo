0056aec50fac2bc3c0cd6312ea3e406a6f0e80e9
João Eurico "Bokomoko" Lima <34620239+Bokomoko@users.noreply.github.com>
2020-12-21 12:25:59 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <div>
+    <h1>What the fuck! this thing works</h1>
+    <p>as armas e os baroes</p>
+  </div>,
+  rootElement
+);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   rootElement
+// );
