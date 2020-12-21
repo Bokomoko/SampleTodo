af6cbe560b9fb464d4df194e56789206e6b75432
João Eurico "Bokomoko" Lima <34620239+Bokomoko@users.noreply.github.com>
2020-12-21 13:48:45 -0300

    Teste de deploy no vercel Modificando o programa no meu editor, commitando para o github e atualizando a aplicação no hospedeiro (vercel). Vamos ver se rola

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import MyList from "./MyList.js";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Greetings Earthlings!</h1>
+      <h2>I come in peace!</h2>
       <MyInfo />
       <MyList />
     </div>
