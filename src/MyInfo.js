859583e182554ee735408c135dfae80bbdc5dd9f
João Eurico "Bokomoko" Lima <34620239+Bokomoko@users.noreply.github.com>
2020-12-21 13:31:08 -0300

    Breaking apart functions Shows how components can be split among many files. Each component, a function in its own .js file

---
+++ src/MyInfo.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function MyInfo() {
+  return (
+    <div>
+      <h1>João Eurico</h1>
+      <h2>aka Boko Moko</h2>
+    </div>
+  );
+}
---
+++ src/MyList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function MyList() {
+  return (
+    <ul>
+      <li>Lara</li>
+      <li>Nani</li>
+      <li>Taís</li>
+    </ul>
+  );
+}
