935772d46d5e83e3a01fa5f21ff58e5355083fe4
João Eurico "Bokomoko" Lima <34620239+Bokomoko@users.noreply.github.com>
2021-01-13 19:45:51 -0300

    Criação de objeto lista componentização tentada. Ainda precisa de alguns ajustes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React from "react";
-import "./styles.css";
+import React, { Component } from "react";
 import MyInfo from "../components/MyInfo.js";
 import MyList from "../components/MyList.js";
 import SuperFooter from "../components/SuperFooter";
-import ListOfGirls from "./LoadGirls";
+import "./styles.css";
 const TheRightStyle = {
   color: "#FF8C00",
   backgroundColor: "#FF2D00",
@@ -15,12 +14,25 @@ const TheMessage = {
   text: "eita ferro"
 };
 
-export default function App() {
-  return (
-    <div className="App">
-      <MyInfo />
-      <MyList />
-      <SuperFooter aMessage={TheMessage} />
-    </div>
-  );
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      girlList: [
+        { name: "Lara", nivel: "rainha", pic: "lara01.png" },
+        { name: "Nani", nivel: "filha", pic: "nani01.png" },
+        { name: "Tais", nivel: "princesa", pic: "tais01.png" }
+      ]
+    };
+  }
+  render() {
+    const { girlList } = this.state
+    return (
+      <div className="App">
+        <MyInfo />
+        <MyList girlList={girlList}/>
+        <SuperFooter aMessage={TheMessage} />
+      </div>
+    );
+  }
 }
